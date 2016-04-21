3aa6d9e95d71a458a8f9e3ccfd1df8739cb3638a
Euku <euku@arcor.de>
2016-04-21 16:19:55 +0000

    +.gitignore; -Uhrzeit bei Backup

--- jobs/create-database-dump.sh
+++ jobs/create-database-dump.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 OUTPUT_DIR="/data/project/mp/public_html/sql-dumps"
-TODAY=`date +"%F_%H-%M"`
+TODAY=`date +%F`
 OUTPUT_FILE="wpmp-$TODAY.sql.bz2"
 mysqldump --defaults-file="/data/project/mp/replica.my.cnf" -h tools-db "s51391__mp" | bzip2 > "$OUTPUT_DIR/$OUTPUT_FILE"
