c3a43ef74037e6dd4939f8eedf228cfce0abdb92
Euku <euku@arcor.de>
2019-02-27 22:56:50 +0000

    migration from 'oursql' to 'pymysql.cursors' and python3

--- pyapi/dewpmp.py
+++ pyapi/dewpmp.py
@@ -8,7 +8,8 @@ class DewpmpException(Exception):
     def __str__(self):
         return repr(self.msg)
 
-import oursql
+# https://pypi.org/project/PyMySQL/#installation
+import pymysql.cursors
 import string
 
 class Database:
@@ -21,39 +22,39 @@ class Database:
         name, e. g. 'dewiki_p'.
         """
         # try to read the replica.mp-db.cnf
-        import ConfigParser
+        import configparser
         import os.path
         import mp_db_config
 
         p = mp_db_config.db_conf_file
         if os.path.exists(p):
-            parser = ConfigParser.ConfigParser()
+            parser = configparser.ConfigParser()
             parser.read(p)
             if parser.has_section('client'):
                 if parser.has_option('client', 'user') and user_name is None:
-                    user_name = string.strip(parser.get('client', 'user'), '"\'')
+                    user_name = parser.get('client', 'user').strip('"\'')
                 if (parser.has_option('client', 'password') and password is None):
-                    password = string.strip(parser.get('client', 'password'), '"\'')
+                    password = parser.get('client', 'password').strip('"\'')
                 if parser.has_option('client', 'host') and host is None:
-                    host = string.strip(parser.get('client', 'host'), '"\'')
+                    host = parser.get('client', 'host').strip('"\'')
                 if parser.has_option('client', 'database') and database is None:
-                    database = string.strip(parser.get('client', 'database'), '"\'')
+                    database = parser.get('client', 'database').strip('"\'')
                 if parser.has_option('client', 'wp_host') and wp_host is None:
-                    wp_host = string.strip(parser.get('client', 'wp_host'), '"\'')
+                    wp_host = parser.get('client', 'wp_host').strip('"\'')
 
         if user_name is None or password is None or host is None:
-            raise DewpmpException(u'You did not specify enough information on' +
-                                u' the database connection. The replica.mp-db.cnf ' +
-                                u'file did not contain the required ' +
-                                u'information. Used user_name: %s, host: %s' % (user_name, host))
+            raise DewpmpException('You did not specify enough information on' +
+                                ' the database connection. The replica.mp-db.cnf ' +
+                                'file did not contain the required ' +
+                                'information. Used user_name: %s, host: %s' % (user_name, host))
 
         try:
-            self.conn = oursql.connect(user=user_name, passwd=password, host=host, db=database)
-            self.conn_wp = oursql.connect(user=user_name, passwd=password, host=wp_host, db=wp_database,
+            self.conn = pymysql.connect(user=user_name, password=password, host=host, db=database)
+            self.conn_wp = pymysql.connect(user=user_name, password=password, host=wp_host, db=wp_database,
                            charset='utf8', use_unicode=True)
-        except oursql.DatabaseError, e:
-            raise DewpmpException(u'You specified wrong database ' +
-                                  u'connection data. Error message: ' + 
+        except pymysql.DatabaseError as e:
+            raise DewpmpException('You specified wrong database ' +
+                                  'connection data. Error message: ' + 
                                   unicode(e))
 
     def get_all_mentors(self):
@@ -127,7 +128,7 @@ class Database:
                    `mentee_user_id`, `mentee_user_name`,
                    `mm_start`, `mm_stop`
                 FROM `mentee`
-                JOIN `mentee_mentor` ON `mentee_user_id`=`mm_mentee_id` AND `mentee_user_name` = ?
+                JOIN `mentee_mentor` ON `mentee_user_id`=`mm_mentee_id` AND `mentee_user_name` = %s
                 JOIN `mentor` ON `mentor_user_id`=`mm_mentor_id`
                 WHERE `mentee_is_hidden`= 0
                 ORDER BY `mentee_user_name`
@@ -162,9 +163,9 @@ class Database:
         mentee_user_id = self.get_mw_user_id(mentee_name)
         mentor_user_id = self.get_mw_user_id(mentor_name)
         if (mentee_user_id == None):
-            raise DewpmpException(u'Coud not find ' + mentee_name + u' in wp_DB')
+            raise DewpmpException('Coud not find ' + mentee_name + ' in wp_DB')
         if (mentor_user_id == None):
-            raise DewpmpException(u'Coud not find mentor: ' + mentor_name + u' in wp_DB')
+            raise DewpmpException('Coud not find mentor: ' + mentor_name + ' in wp_DB')
 
         self._touch_mentor(mentor_name, mentor_user_id)
 
@@ -173,7 +174,7 @@ class Database:
             curs.execute('''
             SELECT `mentee_user_id`, `mentee_user_name`
                 FROM `mentee`
-                WHERE `mentee_user_id` = ?
+                WHERE `mentee_user_id` = %s
             ;''',(mentee_user_id,))
 
             row = curs.fetchone()
@@ -197,14 +198,14 @@ class Database:
                INSERT INTO `mentee_mentor`
                   (`mm_mentee_id`, `mm_mentor_id`,
                    `mm_start`, `mm_stop`, `mm_type`)
-                  VALUE (?, ?, CURRENT_TIMESTAMP, NULL, ?)
+                  VALUE (%s, %s, CURRENT_TIMESTAMP, NULL, %s)
                ;''',(mentee_user_id, mentor_user_id, mentoring_type,))
             else:
                curs.execute('''
                INSERT INTO `mentee_mentor`
                   (`mm_mentee_id`, `mm_mentor_id`,
                    `mm_start`, `mm_stop`, `mm_type`)
-                  VALUE (?, ?, ?, NULL, ?)
+                  VALUE (%s, %s, %s, NULL, %s)
                ;''',(mentee_user_id, mentor_user_id, timestamp, mentoring_type,))
 
     def add_mentee_user(self, mentee_name, mentee_id, timestamp=None):
@@ -217,14 +218,14 @@ class Database:
                INSERT INTO `mentee` (`mentee_user_id`,
                    `mentee_user_name` , `mentee_is_hidden`,
                    `mentee_remark` , `mentee_lastupdate` )
-                   VALUES (?, ?, '0', NULL, CURRENT_TIMESTAMP)
+                   VALUES (%s, %s, '0', NULL, CURRENT_TIMESTAMP)
                ;''', (mentee_id, mentee_name,))
             else:
                curs.execute('''
                INSERT INTO `mentee` (`mentee_user_id`,
                    `mentee_user_name` , `mentee_is_hidden`,
                    `mentee_remark` , `mentee_lastupdate` )
-                   VALUES (?, ?, '0', NULL, ?)
+                   VALUES (%s, %s, '0', NULL, %s)
                ;''', (mentee_id, mentee_name, timestamp,))
         return True
 
@@ -236,7 +237,7 @@ class Database:
         with self.conn as curs:
             curs.execute('''
             SELECT `mentor_user_id`, `mentor_user_name`
-                FROM `mentor` WHERE `mentor_user_id` = ?
+                FROM `mentor` WHERE `mentor_user_id` = %s
             ;''',(mentor_user_id,))
             row = curs.fetchone()
             if row == None:
@@ -248,7 +249,7 @@ class Database:
                      `mentor_in`, `mentor_out`,
                      `mentor_has_barnstar`, `mentor_award_level`,
                      `mentor_remark`, `mentor_lastupdate`) 
-                      VALUES (?, ?, NULL , CURRENT_TIMESTAMP, NULL , 0, 0, NULL , CURRENT_TIMESTAMP)
+                      VALUES (%s, %s, NULL , CURRENT_TIMESTAMP, NULL , 0, 0, NULL , CURRENT_TIMESTAMP)
                       ;''', (mentor_user_id, mentor_name))
             elif (row[1] != mentor_name):
                # we know the id, but not the name
@@ -256,8 +257,8 @@ class Database:
                with self.conn as curs:
                   curs.execute('''
                   UPDATE `mentor`
-                     SET `mentor_user_name` =  ? 
-                     WHERE `mentor_user_id` = ? LIMIT 1
+                     SET `mentor_user_name` =  %s 
+                     WHERE `mentor_user_id` = %s LIMIT 1
                       ;''', (mentor_name, mentor_user_id,))               
  
 
@@ -265,8 +266,8 @@ class Database:
         with self.conn as curs:
             curs.execute('''
             UPDATE `mentee`
-                SET `mentee_user_name` =  ?  
-                WHERE `mentee`.`mentee_user_id` =  ?  LIMIT 1
+                SET `mentee_user_name` =  %s  
+                WHERE `mentee`.`mentee_user_id` =  %s  LIMIT 1
             ;''', (new_mentee_name, mentee_id,))
         return True
 
@@ -278,7 +279,7 @@ class Database:
                curs.execute('''
                UPDATE `mentee_mentor`
                    SET `mm_stop` = CURRENT_TIMESTAMP
-                   WHERE `mm_stop` IS NULL AND `mm_mentee_id` = ?
+                   WHERE `mm_stop` IS NULL AND `mm_mentee_id` = %s
                ;''', (mentee_id,))
 
     def reopen_mm_relation_or_stop(self, mentee_id):
@@ -289,14 +290,14 @@ class Database:
               'stops' the old mentoring, if not already done
         """
         with self.conn as curs:
-            curs.execute('''SELECT COUNT(*) FROM `mentee_mentor` WHERE `mm_mentee_id` = ? AND mm_stop >= DATE_SUB(NOW(), INTERVAL 1 DAY);''', (mentee_id,))
+            curs.execute('''SELECT COUNT(*) FROM `mentee_mentor` WHERE `mm_mentee_id` = %s AND mm_stop >= DATE_SUB(NOW(), INTERVAL 1 DAY);''', (mentee_id,))
             row = curs.fetchone()
             # is there an old m_m relation that was stopped less than 24 hours ago?
             if row != None and row[0] != None and int(row[0]) > 0:
                curs.execute('''
                UPDATE `mentee_mentor`
                    SET `mm_stop` = NULL
-                   WHERE `mm_stop` IS NOT NULL AND mm_stop >= DATE_SUB(NOW(), INTERVAL 1 DAY) AND `mm_mentee_id` = ?
+                   WHERE `mm_stop` IS NOT NULL AND mm_stop >= DATE_SUB(NOW(), INTERVAL 1 DAY) AND `mm_mentee_id` = %s
                ;''', (mentee_id,))
                return True
             else: # stop the last relation
@@ -343,7 +344,7 @@ class Database:
                    `mentee_is_hidden`, `mentee_remark`,
                    `mentee_lastupdate`
                 FROM `mentee`
-                WHERE `mentee_id` = ?
+                WHERE `mentee_id` = %s
             ;''', (mentee_id,))
             if curs.rowcount > 0:
                 item = curs.fetchone()
@@ -365,7 +366,7 @@ class Database:
            this is a workaround, because we don't get correct utf-8 back from dewiki_p
            maybe there is a better way?
         """
-        mapping = ((u"\xc3\u0178", u"ß"), (u'\xc3\xbc', u'ü'), (u'\xc3\xb6', u'ö'), (u'\xc3\xa4', u'ä'),
+        mapping = ((u"\xc3\u0178", u"ß"), ('\xc3\xbc', 'ü'), ('\xc3\xb6', 'ö'), ('\xc3\xa4', 'ä'),
                    (u"\xc3\xa9", u"é"), (u"\xc3\u2013", u"Ö"))
         for m in mapping:
             string = string.replace(m[0], m[1])
@@ -382,7 +383,7 @@ class Database:
         with self.conn_wp as curs:
             curs.execute('''
             SELECT `user_id` FROM `user`
-                WHERE `user_name` = CONVERT(CAST(? AS BINARY) USING latin1)
+                WHERE `user_name` = CONVERT(CAST(%s AS BINARY) USING latin1)
             ;''',(user_name,))
             row = curs.fetchone()
             if row != None and row[0] != None:
@@ -398,7 +399,7 @@ class Database:
             curs.execute('''
             SELECT CONVERT(CAST(`page_title` as BINARY) USING utf8)
                 FROM `page`
-                  JOIN `categorylinks` ON cl_to = ? AND `cl_from`=`page_id` AND `cl_type`="page"
+                  JOIN `categorylinks` ON cl_to = %s AND `cl_from`=`page_id` AND `cl_type`="page"
                 WHERE `page_namespace`=2 ORDER BY `page_title`
             ;''',(cat_name,))
             m_list = curs.fetchall()
@@ -414,9 +415,9 @@ class Database:
         This method is used to identify inactive users.
         """
         with self.conn_wp as curs:
-            curs.execute(u'''
+            curs.execute('''
               select COUNT(rev_id) from revision_userindex join page on (page_id = rev_page) where 
-              rev_user=? and DATEDIFF(NOW(), rev_timestamp) < ?
+              rev_user=%s and DATEDIFF(NOW(), rev_timestamp) < %s
             ;''',(user_id, latest_days,))
             row = curs.fetchone()
             if row != None and row[0] != None:
